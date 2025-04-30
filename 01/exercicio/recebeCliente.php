3c73811db04c8c72919122d1de13ffa4b4a585ca
Gabriel <gabrieusilvaa14@gmail.com>
2025-04-30 13:31:28 -0300

    atividade php

--- 01/exercicio/recebeCliente.php
+++ 01/exercicio/recebeCliente.php
@@ -8,3 +8,8 @@ Telefone
 $nome = $_GET['nome'];
 $email = $_GET['email'];
 $telefone = $_GET['telefone'];
+
+echo "Nome: $nome <br>";
+echo "Email: $email <br>";
+echo "Telefone: $telefone <br>";
+?>
